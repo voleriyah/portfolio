b9ab1fd9875662bb369ba4f9cf29d3532efe8ca9
Valeriya Kostyuchenko <voleriyah@Valeriyas-MacBook-Pro-6.local>
2026-01-05 18:24:33 +0500

    password check

--- app/api/check-password/route.ts
+++ app/api/check-password/route.ts
@@ -4,10 +4,11 @@ import { cookies } from 'next/headers';
 export async function POST(request: Request) {
   const { password } = await request.json();
   
-  if (password === '123456') { // тот же пароль что в middleware
-    cookies().set('case-password', password, {
+  if (password === '123456') {
+    const cookieStore = await cookies(); // добавь await
+    cookieStore.set('case-password', password, {
       httpOnly: true,
-      maxAge: 60 * 60 * 24, // 24 часа
+      maxAge: 60 * 60 * 24,
       path: '/',
     });
     
