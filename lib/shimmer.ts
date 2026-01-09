2cac9581f3616528ef2bdba89270801c1cee9e9b
Valeriya Kostyuchenko <voleriyah@Valeriyas-MacBook-Pro-6.local>
2026-01-09 12:22:07 +0500

    shimmer fix

--- lib/shimmer.ts
+++ lib/shimmer.ts
@@ -2,15 +2,15 @@ export const shimmer = (w: number, h: number) => `
 <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg">
   <defs>
     <linearGradient id="shimmer-gradient" x1="0%" y1="0%" x2="100%" y2="0%">
-      <stop offset="0%" stop-color="#1E1E1E" />
-      <stop offset="20%" stop-color="#2A2A2A" />
-      <stop offset="40%" stop-color="#1E1E1E" />
-      <stop offset="60%" stop-color="#2A2A2A" />
-      <stop offset="80%" stop-color="#1E1E1E" />
-      <stop offset="100%" stop-color="#2A2A2A" />
+      <stop offset="0%" stop-color="#F13729" />
+      <stop offset="20%" stop-color="#F62F20" />
+      <stop offset="40%" stop-color="#F13729" />
+      <stop offset="60%" stop-color="#F62F20" />
+      <stop offset="80%" stop-color="#F13729" />
+      <stop offset="100%" stop-color="#F62F20" />
     </linearGradient>
   </defs>
-  <rect width="${w}" height="${h}" fill="#1E1E1E" />
+  <rect width="${w}" height="${h}" fill="#F62F20" />
   <rect width="${w}" height="${h}" fill="url(#shimmer-gradient)" />
 </svg>`
 
